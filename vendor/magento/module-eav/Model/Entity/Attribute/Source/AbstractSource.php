93974852076064251891be5b6b1407ae356bc681
inkxe user <inkxe@s132-148-86-33.secureserver.net>
2017-08-22 10:53:37 -0700

    mobile style improvements

--- vendor/magento/module-eav/Model/Entity/Attribute/Source/AbstractSource.php
+++ vendor/magento/module-eav/Model/Entity/Attribute/Source/AbstractSource.php
@@ -78,13 +78,13 @@ abstract class AbstractSource implements
     }
 
     /**
-     * @param string $value
+     * @param string|array $value
      * @return null|string
      */
     public function getOptionId($value)
     {
         foreach ($this->getAllOptions() as $option) {
-            if (strcasecmp($option['label'], $value) == 0 || $option['value'] == $value) {
+            if ($option['value'] == $value) {
                 return $option['value'];
             }
         }
